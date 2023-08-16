25bb0c1d8ae2e24b0bdad5ace4f4d2f7075b7aef
nirdoshyaqin <himalayanowhere@gmail.com>
2023-08-16 11:31:40 +0900

    一時的にdonationページへのメニュー項目をコメントアウト

--- components/header.js
+++ components/header.js
@@ -34,11 +34,11 @@ function Header() {
               </Link>
             ))}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <Link href="/donate" className="text-sm font-semibold leading-6 text-gray-900">
               Donate <span aria-hidden="true">&rarr;</span>
             </Link>
-          </div>
+          </div> */}
         </nav>
       </header>
     </>
