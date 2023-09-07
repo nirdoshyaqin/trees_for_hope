900df805204e485875d211ef1b9b9c0718937f6a
nirdoshyaqin <himalayanowhere@gmail.com>
2023-09-07 22:24:38 +0900

    ヘッダー、フッターのナビメニュー項目に「Tree Planting」を追加

--- components/footer.js
+++ components/footer.js
@@ -7,6 +7,7 @@ const footerNavigation = {
     { name: "Mission", href: "/mission" },
     { name: "Projects", href: "/projects" },
     { name: "Team", href: "/team" },
+    { name: "Tree Planting", href: "/tree_planting" },
     { name: "Contact", href: "/contact" }
   ],
   social: [
