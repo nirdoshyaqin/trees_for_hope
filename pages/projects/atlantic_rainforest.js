ad9339af808291c02d639f18e16be35a2a93ec2e
nirdoshyaqin <himalayanowhere@gmail.com>
2023-09-08 21:19:08 +0900

    フォームの項目の調整

--- pages/projects/atlantic_rainforest.js
+++ pages/projects/atlantic_rainforest.js
@@ -54,8 +54,8 @@ export default function Project() {
               />
             </div>
             <div className="overflow-hidden">
-              <div className="mx-auto max-w-7xl px-6 pb-32 mt-32 sm:mt-40 lg:px-8">
-                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+              <div className="mx-auto max-w-3xl px-6 pb-32 mt-40 lg:px-8">
+                <div className="gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                   <div className="prose w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                     <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Reviving the Atlantic Rainforest</h1>
                     <h2>Community and Rainforests Engagement Weeks (CREW)</h2>
