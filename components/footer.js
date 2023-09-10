6ce2c1f6352ce504978d336344bd0aa338c61215
nirdoshyaqin <himalayanowhere@gmail.com>
2023-09-10 16:49:57 +0900

    フッターメニュー項目smooth-scrollの調整

--- components/footer.js
+++ components/footer.js
@@ -67,7 +67,7 @@ function Footer() {
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {footerNavigation.main.map(item => (
             <div key={item.name} className="pb-6">
-              <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+              <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900" scroll={false}>
                 {item.name}
               </Link>
             </div>
