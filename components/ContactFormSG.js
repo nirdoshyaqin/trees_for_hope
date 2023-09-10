e5e4429ed89bc1473736b9116105d3b821c5fdd2
nirdoshyaqin <himalayanowhere@gmail.com>
2023-09-10 18:59:57 +0900

    SP表示時にinputの枠線が消えていたので、調整。

--- components/ContactFormSG.js
+++ components/ContactFormSG.js
@@ -106,7 +106,7 @@ export default function contactFormSG() {
                   First name
                 </label>
                 <div className="mt-2.5">
-                  <input type="text" name="firstname" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  <input type="text" name="firstname" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
               </div>
               <div>
@@ -114,7 +114,7 @@ export default function contactFormSG() {
                   Last name
                 </label>
                 <div className="mt-2.5">
-                  <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
               </div>
               {/* <div>
@@ -122,7 +122,7 @@ export default function contactFormSG() {
                       Company
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="company" id="company" autoComplete="family-name" value={company} placeholder="Haghighi" onChange={e => setCompany(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="company" id="company" autoComplete="family-name" value={company} placeholder="Haghighi" onChange={e => setCompany(e.target.value)} className="block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div> */}
 
@@ -131,7 +131,7 @@ export default function contactFormSG() {
                   Email
                 </label>
                 <div className="mt-2.5">
-                  <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
               </div>
 
@@ -140,7 +140,7 @@ export default function contactFormSG() {
                   Phone
                 </label>
                 <div className="mt-2.5">
-                  <input type="tel" name="phone" id="phone" autoComplete="phone" value={phone} placeholder="phone number" pattern="[\d\-]*" onChange={e => setPhone(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  <input type="tel" name="phone" id="phone" autoComplete="phone" value={phone} placeholder="phone number" pattern="[\d\-]*" onChange={e => setPhone(e.target.value)} className="block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
               </div>
               <div className="sm:col-span-2">
@@ -148,7 +148,7 @@ export default function contactFormSG() {
                   Message
                 </label>
                 <div className="mt-2.5">
-                  <textarea name="message" id="message" rows={4} value={message} onChange={e => setMessage(e.target.value)} placeholder="message" required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                  <textarea name="message" id="message" rows={4} value={message} onChange={e => setMessage(e.target.value)} placeholder="message" required className="block w-full rounded-md px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                 </div>
               </div>
               <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
