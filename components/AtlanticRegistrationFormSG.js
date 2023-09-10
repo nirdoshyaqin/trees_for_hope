0db704e54fbb8ff0b028ca486546984ef8573e5b
nirdoshyaqin <himalayanowhere@gmail.com>
2023-09-10 21:04:24 +0900

    枠線の微調整

--- components/AtlanticRegistrationFormSG.js
+++ components/AtlanticRegistrationFormSG.js
@@ -116,7 +116,7 @@ export default function AtlanticRegistrationFormSG() {
                       First name
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="first-name" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="first-name" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div>
@@ -124,7 +124,7 @@ export default function AtlanticRegistrationFormSG() {
                       Last name
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -132,7 +132,7 @@ export default function AtlanticRegistrationFormSG() {
                       Email
                     </label>
                     <div className="mt-2.5">
-                      <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
 
@@ -141,7 +141,7 @@ export default function AtlanticRegistrationFormSG() {
                       Phone
                     </label>
                     <div className="mt-2.5">
-                      <input type="tel" name="phone" id="phone" autoComplete="phone" value={phone} placeholder="phone number" pattern="[\d\-]*" onChange={e => setPhone(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="tel" name="phone" id="phone" autoComplete="phone" value={phone} placeholder="phone number" pattern="[\d\-]*" onChange={e => setPhone(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -149,7 +149,7 @@ export default function AtlanticRegistrationFormSG() {
                       Why do you want to join this week ?
                     </label>
                     <div className="mt-2.5">
-                      <textarea type="text" name="reason" id="reason" rows={4} autoComplete="organization" value={reason} placeholder="Trees for Hope" onChange={e => setReason(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <textarea type="text" name="reason" id="reason" rows={4} autoComplete="organization" value={reason} placeholder="Trees for Hope" onChange={e => setReason(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -157,7 +157,7 @@ export default function AtlanticRegistrationFormSG() {
                       Your next of kin and his / her phone number
                     </label>
                     <div className="mt-2.5">
-                      <input type="tel" name="kinphone" id="kinphone" autoComplete="organization" value={kinphone} placeholder="Your next of kin and his / her phone number" pattern="[\d\-]*" onChange={e => setKinphone(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="tel" name="kinphone" id="kinphone" autoComplete="organization" value={kinphone} placeholder="Your next of kin and his / her phone number" pattern="[\d\-]*" onChange={e => setKinphone(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -165,7 +165,7 @@ export default function AtlanticRegistrationFormSG() {
                       What are your expectations from this programme?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="expectation" id="expectation" rows={4} value={expectation} onChange={e => setExpectation(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="expectation" id="expectation" rows={4} value={expectation} onChange={e => setExpectation(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -173,7 +173,7 @@ export default function AtlanticRegistrationFormSG() {
                       Do you have any special dietary needs ?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="dietary" id="dietary" rows={4} value={dietary} onChange={e => setDietary(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="dietary" id="dietary" rows={4} value={dietary} onChange={e => setDietary(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
 
@@ -182,7 +182,7 @@ export default function AtlanticRegistrationFormSG() {
                       Any other comment ?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="message" id="message" rows={4} value={message} onChange={e => setMessage(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="message" id="message" rows={4} value={message} onChange={e => setMessage(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
--- components/TreeRegistrationFormSG.js
+++ components/TreeRegistrationFormSG.js
@@ -122,7 +122,7 @@ export default function TreeRegistrationFormSG() {
                       First name
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="first-name" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="first-name" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div>
@@ -130,7 +130,7 @@ export default function TreeRegistrationFormSG() {
                       Last name
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -138,7 +138,7 @@ export default function TreeRegistrationFormSG() {
                       Email
                     </label>
                     <div className="mt-2.5">
-                      <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
 
@@ -147,7 +147,7 @@ export default function TreeRegistrationFormSG() {
                       Company
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="company" id="company" autoComplete="organization" value={company} placeholder="Trees for Hope" onChange={e => setCompany(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="company" id="company" autoComplete="organization" value={company} placeholder="Trees for Hope" onChange={e => setCompany(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -155,7 +155,7 @@ export default function TreeRegistrationFormSG() {
                       Dedication
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="dedication_for" id="dedication_for" autoComplete="organization" value={dedication_for} placeholder="Trees for Hope" onChange={e => setDedication_for(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="dedication_for" id="dedication_for" autoComplete="organization" value={dedication_for} placeholder="Trees for Hope" onChange={e => setDedication_for(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -163,7 +163,7 @@ export default function TreeRegistrationFormSG() {
                       Numbers of Trees
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="numbers_of_trees" id="numbers_of_trees" autoComplete="organization" value={numbers_of_trees} placeholder="1" onChange={e => setNumbers_of_trees(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="numbers_of_trees" id="numbers_of_trees" autoComplete="organization" value={numbers_of_trees} placeholder="1" onChange={e => setNumbers_of_trees(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -171,7 +171,7 @@ export default function TreeRegistrationFormSG() {
                       images ( Limit 10MB )
                     </label>
                     <div className="mt-2.5">
-                      <input type="file" name="images" id="images" autoComplete="organization" value={images} placeholder="1" onChange={e => setImages(e.target.value)} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="file" name="images" id="images" autoComplete="organization" value={images} placeholder="1" onChange={e => setImages(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -179,7 +179,7 @@ export default function TreeRegistrationFormSG() {
                       Message
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="message" id="message" rows={4} value={message} onChange={e => setMessage(e.target.value)} required className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="message" id="message" rows={4} value={message} onChange={e => setMessage(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
