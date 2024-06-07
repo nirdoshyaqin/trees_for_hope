1047b528c718fbe0455c1dcd704b0b7478826fd1
nirdoshyaqin <himalayanowhere@gmail.com>
2024-06-07 09:39:49 +0900

    必要項目マークのアスタリスクをトル

--- components/AtlanticRegistrationFormSG.js
+++ components/AtlanticRegistrationFormSG.js
@@ -203,7 +203,7 @@ export default function AtlanticRegistrationFormSG() {
                   </div>
                   <div className="sm:col-span-2">
                     <label htmlFor="contribution" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Have you paid for the week? How much are you contributing? *
+                      Have you paid for the week? How much are you contributing?
                     </label>
                     <div className="mt-2.5">
                       <textarea name="contribution" id="contribution" rows={4} value={contribution} onChange={e => setContribution(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
