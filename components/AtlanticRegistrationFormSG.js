78d952c92f3cf3ea85cbcfcbb97009705affbda0
nirdoshyaqin <himalayanowhere@gmail.com>
2024-06-07 09:27:17 +0900

    required field 必須項目の変更

--- components/AtlanticRegistrationFormSG.js
+++ components/AtlanticRegistrationFormSG.js
@@ -132,15 +132,15 @@ export default function AtlanticRegistrationFormSG() {
                       Last name *
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
                     <label htmlFor="dateplaceofbirth" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Date and Place of Birth *
+                      Date and Place of Birth
                     </label>
                     <div className="mt-2.5">
-                      <input type="dateplaceofbirth" name="dateplaceofbirth" id="dateplaceofbirth" autoComplete="dateplaceofbirth" value={dateplaceofbirth} onChange={e => setDateplaceofbirth(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="dateplaceofbirth" name="dateplaceofbirth" id="dateplaceofbirth" autoComplete="dateplaceofbirth" value={dateplaceofbirth} onChange={e => setDateplaceofbirth(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -154,15 +154,15 @@ export default function AtlanticRegistrationFormSG() {
 
                   <div className="sm:col-span-2">
                     <label htmlFor="phone" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Phone *
+                      Phone
                     </label>
                     <div className="mt-2.5">
-                      <input type="tel" name="phone" id="phone" autoComplete="phone" value={phone} placeholder="phone number" pattern="[\d\-]*" onChange={e => setPhone(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="tel" name="phone" id="phone" autoComplete="phone" value={phone} placeholder="phone number" pattern="[\d\-]*" onChange={e => setPhone(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
                     <label htmlFor="kinphone" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Your next of kin and his / her phone number *
+                      Your next of kin and his / her phone number
                     </label>
                     <div className="mt-2.5">
                       <input type="tel" name="kinphone" id="kinphone" autoComplete="organization" value={kinphone} placeholder="Your next of kin and his / her phone number" pattern="[\d\-]*" onChange={e => setKinphone(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
@@ -171,18 +171,18 @@ export default function AtlanticRegistrationFormSG() {
 
                   <div className="sm:col-span-2">
                     <label htmlFor="reason" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Why do you want to join this week ? *
+                      Why do you want to join this week ?
                     </label>
                     <div className="mt-2.5">
-                      <textarea type="text" name="reason" id="reason" rows={4} autoComplete="organization" value={reason} placeholder="Trees for Hope" onChange={e => setReason(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <textarea type="text" name="reason" id="reason" rows={4} autoComplete="organization" value={reason} placeholder="Trees for Hope" onChange={e => setReason(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
                     <label htmlFor="expectation" className="block text-sm font-semibold leading-6 text-gray-900">
-                      What are your expectations from this programme? *
+                      What are your expectations from this programme?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="expectation" id="expectation" rows={4} value={expectation} onChange={e => setExpectation(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="expectation" id="expectation" rows={4} value={expectation} onChange={e => setExpectation(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -190,7 +190,7 @@ export default function AtlanticRegistrationFormSG() {
                       Do you have any special needs we should know about?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="specialneed" id="specialneed" rows={4} value={specialneed} onChange={e => setSpecialneed(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="specialneed" id="specialneed" rows={4} value={specialneed} onChange={e => setSpecialneed(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -198,7 +198,7 @@ export default function AtlanticRegistrationFormSG() {
                       Do you have any special dietary needs ?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="dietary" id="dietary" rows={4} value={dietary} onChange={e => setDietary(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="dietary" id="dietary" rows={4} value={dietary} onChange={e => setDietary(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -206,15 +206,15 @@ export default function AtlanticRegistrationFormSG() {
                       Have you paid for the week? How much are you contributing? *
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="contribution" id="contribution" rows={4} value={contribution} onChange={e => setContribution(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="contribution" id="contribution" rows={4} value={contribution} onChange={e => setContribution(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
                     <label htmlFor="howdoyouhear" className="block text-sm font-semibold leading-6 text-gray-900">
-                      How did you hear about this week? *
+                      How did you hear about this week?
                     </label>
                     <div className="mt-2.5">
-                      <textarea name="howdoyouhear" id="howdoyouhear" rows={4} value={howdoyouhear} onChange={e => setHowdoyouhear(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                      <textarea name="howdoyouhear" id="howdoyouhear" rows={4} value={howdoyouhear} onChange={e => setHowdoyouhear(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
                     </div>
                   </div>
                   <div className="sm:col-span-2">
