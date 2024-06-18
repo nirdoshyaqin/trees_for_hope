790b2de692a812b8736beaaf868e7e6e995b21e6
nirdoshyaqin <himalayanowhere@gmail.com>
2024-06-18 21:32:47 +0900

    Created "Rewilding path Berlin Page"

---
+++ components/RewildingBerlinRegistFormSG.js
@@ -0,0 +1,213 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import Head from "next/head";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Switch } from "@headlessui/react";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function TreeRegistrationFormSG() {
+  const [agreed, setAgreed] = useState(false);
+
+  const router = useRouter();
+
+  // 項目を設定
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [date_of_birth, setDate_of_birth] = useState("");
+  const [reason, setReason] = useState("");
+  const [special_need, setSpecial_need] = useState("");
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = async e => {
+    // デフォルトの動作をブロック
+    e.preventDefault();
+
+    // let img = document.getElementById("images");
+    // let imgfile = img.files[0];
+
+    // console.log( imgfile );
+
+    // let reader = new FileReader();
+    // reader.onload = function (e) {
+    //   console.log( e.target.result );
+    // }
+    // reader.readAsDataURL(imgfile);
+
+    // 画像の読み込みをawaitで待つ
+    let img = document.getElementById("images");
+    let imgfile = img.files[0];
+    imgfile = await new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = function(e) {
+        resolve(e.target.result);
+      };
+      reader.readAsDataURL(imgfile);
+    });
+    // console.log(imgfile);
+    // data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/....
+
+    // 画像サイズをチェック(10MBまで)
+    if (imgfile.length > 10000000) {
+      alert("The image size is too large. Please select an image of 10MB or less.");
+      return;
+    }
+
+    // 内部の送信用APIへ
+    const res = await fetch("/api/send", {
+      // body: JSON.stringify(e),
+      body: JSON.stringify({
+        lastname: lastname,
+        firstname: firstname,
+        email: email,
+        phone: phone,
+        date_of_birth: date_of_birth,
+        reason: reason,
+        special_need: special_need,
+        comment: comment
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+    });
+
+    const result = await res.json();
+    console.log(result);
+
+    if (result.status == 200) {
+      // メール送信成功
+      alert("Email sent successfully.");
+    }
+
+    // フォーム送信へ
+    // await submit_hubspot_form(firstname, lastname, dedication_for, numbers_of_trees, email, company, images, message);
+  };
+
+  return (
+    <>
+      <div className="bg-white">
+        <main className="isolate">
+          {/* Hero section */}
+          <div className="relative isolate -z-10">
+            <div className="isolate bg-white px-6 mt-40 lg:px-8">
+              <div className="mx-auto max-w-2xl text-center">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Registration Form</h2>
+                {/* <p className="mt-2 text-lg leading-8 text-gray-600">Let us hear from you. We’ll get back to you as soon as we can. Thank you.</p> */}
+              </div>
+              <form className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit}>
+                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                  <div>
+                    <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                      First name
+                    </label>
+                    <div className="mt-2.5">
+                      <input type="text" name="first-name" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                  </div>
+                  <div>
+                    <label htmlFor="lastname" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Last name
+                    </label>
+                    <div className="mt-2.5">
+                      <input type="text" name="lastname" id="lastname" autoComplete="family-name" value={lastname} placeholder="Haghighi" onChange={e => setLastname(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Email
+                    </label>
+                    <div className="mt-2.5">
+                      <input type="email" name="email" id="email" autoComplete="email" value={email} onChange={e => setEmail(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-2">
+                    <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Phone Number
+                    </label>
+                    <div className="mt-2.5">
+                      <input type="text" name="phone" id="phone" autoComplete="organization" value={phone} placeholder="" onChange={e => setPhone(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="dedication_for" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Your date and place of birth
+                    </label>
+                    <div className="mt-2.5">
+                      <input type="text" name="date_of_birth" id="date_of_birth" autoComplete="organization" value={date_of_birth} placeholder="Trees for Hope" onChange={e => setDate_of_birth(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="numbers_of_trees" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Why do you want to join this workshop?
+                    </label>
+                    <div className="mt-2.5">
+                      <input type="text" name="reason" id="reason" autoComplete="organization" value={reason} placeholder="1" onChange={e => setReason(e.target.value)} className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="special_need" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Special Need
+                    </label>
+                    <div className="mt-2.5">
+                      <textarea name="special_need" id="special_need" rows={4} value={special_need} onChange={e => setSpecial_need(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                    </div>
+                  </div>
+                  <div className="sm:col-span-2">
+                    <label htmlFor="comment" className="block text-sm font-semibold leading-6 text-gray-900">
+                      Any other comment ?
+                    </label>
+                    <div className="mt-2.5">
+                      <textarea name="comment" id="comment" rows={4} value={comment} onChange={e => setComment(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
+                    </div>
+                  </div>
+
+                  <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+                    <div className="flex h-6 items-center">
+                      <Switch checked={agreed} onChange={setAgreed} className={classNames(agreed ? "bg-indigo-600" : "bg-gray-200", "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600")}>
+                        <span className="sr-only">Agree to policies</span>
+                        <span aria-hidden="true" className={classNames(agreed ? "translate-x-3.5" : "translate-x-0", "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out")} />
+                      </Switch>
+                    </div>
+                    <Switch.Label className="text-sm leading-6 text-gray-600">
+                      By selecting this, you agree to our{" "}
+                      <a href="/privacy_policy" className="font-semibold text-indigo-600">
+                        privacy&nbsp;policy
+                      </a>
+                      .
+                    </Switch.Label>
+                  </Switch.Group>
+                </div>
+                <div className="mt-10">
+                  <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ pages/rewilding_path_in_berlin.js
@@ -0,0 +1,95 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import RewildingBerlinRegistFormSG from "@/components/RewildingBerlinRegistFormSG";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+const imageLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function TreePlanting() {
+  const [agreed, setAgreed] = useState(false);
+  return (
+    <>
+      <Head>
+        <title>Cultivating Your Rewilding Path Workshop | Trees for Hope</title>
+        <meta name="We offer the processes, skills, tools and means needed to initiate the ecological recovery and personal, social transformations. " content="Trees for Hope Website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-white">
+        {/* Header */}
+        <Header />
+        <main className="isolate">
+          <div className="relative isolate -z-10">
+            <svg className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]" aria-hidden="true">
+              <defs>
+                <pattern id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84" width={200} height={200} x="50%" y={-1} patternUnits="userSpaceOnUse">
+                  <path d="M.5 200V.5H200" fill="none" />
+                </pattern>
+              </defs>
+              <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+                <path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" strokeWidth={0} />
+              </svg>
+              <rect width="100%" height="100%" strokeWidth={0} fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
+            </svg>
+            <div className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48" aria-hidden="true">
+              <div
+                className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                style={{
+                  clipPath: "polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)"
+                }}
+              />
+            </div>
+            <div className="overflow-hidden">
+              <div className="mx-auto max-w-3xl px-6 mt-32 sm:mt-40 lg:px-8">
+                <figure className="mt-10 mb-6 flex justify-center">
+                  <img className="rounded-xl bg-gray-50 object-cover" src="/hives/alchemising_our_soil_soul_society.png" alt="Alchemising our Soil, Soul, Society" />
+                  {/* <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
+                  <InformationCircleIcon className="mt-0.5 h-5 w-5 flex-none text-gray-300" aria-hidden="true" />
+                  Faucibus commodo massa rhoncus, volutpat.
+                </figcaption> */}
+                </figure>
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  Cultivating Your Rewilding Path Workshop
+                  <br />
+                  20, 21, 22th September ​Berlin
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">This workshop is for those who wish to start and deepen their journey in the field of ecological restoration, earth healing, or rewilding. ​Now in the era of climate disruption, many of us are seeking to change our relationship with nature and to contribute to its recovery.</p>
+                <p className="mt-6 text-lg leading-8 text-gray-600">We encourage you to bring your concerns, questions, passions, visions, dreams, actions. We will be weaving a field of connections between the participants, allowing ourselves to deepen in our rewilding journeys.</p>
+                <p className="mt-6 text-lg leading-8 text-gray-600">We expect this workshop will spark a new fire in your life. The fire of your rewilding journey could be tended by your commitment to the next steps on the path. You may be met by a landscape, a place that you recognise as kin. A rewilding journey is about having a deepening relationship with a place that you know as home.</p>
+                <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Price and Payment</h2>
+                <p className="mt-6 text-lg leading-8 text-gray-600">We offer three tiers of price and payment</p>
+                <ul>
+                  <li>【Basic】for those with minimum income : €70</li>
+                  <li>【Full】for those who can pay the ticket : €170</li>
+                  <li>【Giving】for those who would like to support our Rewilding Projects : €270</li>
+                </ul>
+                <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+                  <figure className="mt-10 mb-16">
+                    <Image loader={imageLoader} src="/hives/hives_01.png" className="rounded-xl bg-gray-50" alt="Hives Image" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={768} height={1024} />
+                  </figure>
+                  <figure className="mt-10 mb-16">
+                    <Image loader={imageLoader} src="/hives/hives_02.png" className="rounded-xl bg-gray-50" alt="Hives Image" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" width={768} height={1024} />
+                  </figure>
+                </div>
+              </div>
+              <RewildingBerlinRegistFormSG />
+            </div>
+          </div>
+        </main>{" "}
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
+  );
+}
