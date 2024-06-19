222b57b589f6db93bd08a7a1c570a3d5589fbc6d
nirdoshyaqin <himalayanowhere@gmail.com>
2024-06-19 20:38:49 +0900

    デバッグ対応

--- components/HivesRegistrationFormSG.js
+++ components/HivesRegistrationFormSG.js
@@ -23,7 +23,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function TreeRegistrationFormSG() {
+export default function HivesRegistrationFormSG() {
   const [agreed, setAgreed] = useState(false);
 
   const router = useRouter();
