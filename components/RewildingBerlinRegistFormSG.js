e6225d4b8f4be23a32b78e8dd87da4e5963ccd53
nirdoshyaqin <himalayanowhere@gmail.com>
2024-06-23 14:39:42 +0900

    微調整

--- components/RewildingBerlinRegistFormSG.js
+++ components/RewildingBerlinRegistFormSG.js
@@ -87,11 +87,11 @@ export default function RewildingBerlinRegistFormSG() {
               <form className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit}>
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                   <div>
-                    <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                    <label htmlFor="firstname" className="block text-sm font-semibold leading-6 text-gray-900">
                       First name
                     </label>
                     <div className="mt-2.5">
-                      <input type="text" name="first-name" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input type="text" name="firstname" id="firstname" autoComplete="given-name" value={firstname} placeholder="Pupak" onChange={e => setFirstname(e.target.value)} required className="block w-full rounded-md border-2 md:border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                   </div>
                   <div>
